1faa9a8f14e114185944c4a5dff00d2c7ed687fb
FreezyExp <freezydev@live.nl>
2015-01-19 22:19:44 +0100

    Added Canvas Scale Broadcaster
    Send SetDirty to all components when the screen is scaling.
    This resolves the problem that there is no such event in the UI system.
    
    You can change or add messages being sent on scaling.
    
    It's a quick hack.
    Performance might be terrible, as it uses a BroadcastMessage to send the
    message out.

---
+++ _uGUI/CanvasScaleBroadcaster/Editor/CanvasScaleBroadcasterInspector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor( typeof( CanvasScaleBroadcast ), false )]
+[CanEditMultipleObjects]
+public class CanvasScaleBroadcasterInspector : Editor {
+    //SerializedProperty m_Messages;
+
+    //protected virtual void OnEnable() {
+    //    m_Messages = serializedObject.FindProperty( "m_Messages" );
+    //}
+
+    //public override void OnInspectorGUI() {
+    //    serializedObject.Update();
+    //    DrawDefaultInspector();        
+    //    EditorGUILayout.PropertyField( m_Messages, true );
+    //    serializedObject.ApplyModifiedProperties();
+    //}
+}
