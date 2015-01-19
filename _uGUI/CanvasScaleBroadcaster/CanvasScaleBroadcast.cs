8eeef2b06732ee8e8c30ff36b655284cd7a1212a
FreezyExp <freezydev@live.nl>
2015-01-19 22:22:31 +0100

    Actually added the Scale Broadcaster
    The editor is just there to make sure m_Messages gets serialized.

---
+++ _uGUI/CanvasScaleBroadcaster/CanvasScaleBroadcast.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[AddComponentMenu( "Layout/CanvasScaleBroadcaster" )]
+[ExecuteInEditMode]
+[RequireComponent( typeof( Canvas ) )]
+public class CanvasScaleBroadcast : CanvasScaler {
+
+    [SerializeField]
+    public string[] m_Messages = new string[] { "SetDirty" };
+
+    protected override void Handle() {
+        base.Handle();
+
+        foreach( string msg in m_Messages )
+            BroadcastMessage( msg, SendMessageOptions.DontRequireReceiver );
+    }
+}
